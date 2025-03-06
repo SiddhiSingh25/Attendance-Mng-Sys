d1ade3f1df3253621a5b156ed496dc506d25ccb9
tannu843 <tannu799154@gmail.com>
2025-03-05 23:28:44 -0800

    commit changes

--- Frontend/src/Auth/StudentRegister.jsx
+++ Frontend/src/Auth/StudentRegister.jsx
@@ -1,14 +1,74 @@
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 
 function StudentRegister() {
 
   return (
     <>
-    <h1>Student Register</h1>
+
+      
+      <div className="container-fluid py-5 ">
+        <div className="row">
+          <div className="col-sm-2"></div>
+          <div className="col-sm-8">
+            <div className="regcontainerstd px-3 py-4mt-5">
+
+              <div className='regicon mt-5 '>
+                <FontAwesomeIcon icon={faUser} />
+              </div>
+              <h4 className="regstdheading ">Student registration</h4>
+              <div className="row">
+                <div className="col-sm-1"></div>
+                 <div className="col-sm-10">
+                  <input type="text"
+                    placeholder="Enter you name"
+                    className="form-control rounded-5 mt-3" />
+
+                  <input type="text"
+                    placeholder="Year/Semester"
+                    className="form-control rounded-5 mt-3" />
+
+                    <input type="email"
+                    placeholder="Email.."
+                    className="form-control rounded-5 mt-3" />
+
+                  <input type="text"
+                    placeholder="Branch"
+                    className="form-control rounded-5 mt-3" />
+
+                    <input type="number"
+                    placeholder="Contact no."
+                    className="form-control rounded-5 mt-3" />
+
+                  <input type="password"
+                    placeholder="Password"
+                    className="form-control rounded-5 mt-3" />
+
+                    <input type="password"
+                    placeholder="Confirm-Password"
+                    className="form-control rounded-5 mt-3" />
+
+                    <input type="file"
+                    placeholder="upload you photo"
+                    className="mt-3"/>
+                     <div className="text-centre mt-2 pt-2 ">
+                    <button type="button" class="btn btn-outline-dark">Student Login</button>
+                  </div>
+                </div> 
+                <div className="col-sm-1"></div>
+              </div>
+            </div>
+
+            <div className="col-sm-2"></div>
+          </div >
+        </div>
+      </div>
     </>
-  )
+   )
+
 }
 
-export default StudentRegister
+      export default StudentRegister
--- Frontend/src/Auth/TeacherRegister.jsx
+++ Frontend/src/Auth/TeacherRegister.jsx
@@ -1,14 +1,80 @@
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 
 function TeacherRegister() {
 
   return (
     <>
-    <h1>TeacherRegister</h1>
+
+
+      <div className="container-fluid py-5 mt-5 ">
+        <div className="row">
+          <div className="col-sm-2"></div>
+          <div className="col-sm-8">
+            <div className="regcontainerstd px-3 py-4">
+
+              <div className='regicon mt-5 '>
+                <FontAwesomeIcon icon={faUser} />
+              </div>
+              <h4 className="regstdheading ">Teacher Registration</h4>
+              <div className="row">
+                
+                <div className="col-sm-1"></div>
+                <div className="col-sm-10">
+                  <input type="text"
+                    placeholder="Enter you name"
+                    className="form-control rounded-5 mt-4" />
+
+
+
+                  <input type="email"
+                    placeholder="Email.."
+                    className="form-control rounded-5 mt-4" />
+
+
+                  <select name="branches" className="form-control rounded-5 mt-4">
+                    <option value="branches">branches</option>
+                    <option value="non-technical branches">Non-technical branches</option>
+                    <option value="technical branches">Technical branches</option>
+                  </select>
+                  <input type="number"
+                    placeholder="Contact no."
+                    className="form-control rounded-5 mt-4" />
+                
+                
+                  <input type="password"
+                    placeholder="Password"
+                    className="form-control rounded-5 mt-4" />
+
+                  <input type="password"
+                    placeholder="Confirm-Password"
+                    className="form-control rounded-5 mt-4" />
+
+                  <input type="file"
+                    placeholder="upload you photo"
+                    className="mt-3" />
+                
+                <div className="text-centre mt-2 pt-2 ">
+                  <button type="button" class="btn btn-outline-dark">Submit</button>
+                </div>
+              </div>
+              </div>
+              <div className="col-sm-1"></div>
+            </div>
+          </div>
+
+          <div className="col-sm-2"></div>
+        </div >
+      </div>
+      
+    
     </>
-  )
+   )
+
 }
 
 export default TeacherRegister
+
